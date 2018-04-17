7f880cf446f62eb481de0f0d7dedf01efdc73bce
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2018-04-17 22:24:05 +0100

    add gbt_prompt_forward

---
+++ zshrc.d/exports/gbt_prompt_forward.zsh
@@ -0,0 +1,8 @@
+export GBT__HOME="$GOPATH/src/github.com/jtyr/gbt"
+source "$GBT__HOME/sources/prompt_forwarding/local"
+alias docker='gbt_docker'
+alias mysql='gbt_mysql'
+alias ssh='gbt_ssh'
+alias su='gbt_su'
+alias sudo='gbt_sudo'
+alias vagrant='gbt_vagrant'
