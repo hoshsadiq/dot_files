ce1d3df1acc55cc276b009b330721e3ee151ba4d
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2018-04-20 00:20:33 +0100

    Add signing

--- zshrc.d/exports/misc.zsh
+++ zshrc.d/exports/misc.zsh
@@ -8,3 +8,6 @@ export DISPLAY=:0.0
 # Highlight less
 export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 export LESS=' -R'
+
+# GPG Signing git commits
+export GPG_TTY=$(tty)
