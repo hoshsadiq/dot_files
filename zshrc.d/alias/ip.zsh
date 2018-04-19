35601ae2f5fb2268bf07e1a24ab7c0e1520c9324
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2018-04-19 09:02:45 +0100

    add localip for macos

--- zshrc.d/alias/ip.zsh
+++ zshrc.d/alias/ip.zsh
@@ -9,3 +9,22 @@
 # fi
 alias extip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias ipinfo='curl ifconfig.me/all'
+
+localip() {
+  while read line; do
+      sdev=$(echo $line | awk -F  "(, )|(: )|[)]" '{print $4}')
+      if [ -n "$sdev" ]; then
+          ifout="$(ifconfig $sdev 2>/dev/null)"
+          if echo "$ifout" | grep 'status: active' > /dev/null 2>&1; then
+              ipAddress=$(echo "$ifout" | awk '/inet /{print $2}')
+          fi
+      fi
+  done <<< "$(networksetup -listnetworkserviceorder | grep 'Hardware Port')"
+
+  if [ -n "$ipAddress" ]; then
+      echo $ipAddress
+  else
+      >&2 echo "Could not find active ipaddress"
+      return 1
+  fi
+}
