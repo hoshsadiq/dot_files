2d8702a70a974008b64a72a7611b21fa3bee42ba
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2018-04-24 13:14:57 +0100

    Update paths, add install apps for macos

--- zshrc.d/001-paths.zsh
+++ zshrc.d/001-paths.zsh
@@ -1,10 +1,24 @@
 export GOPATH="$HOME/Workspace"
 export PYENV="$GOPATH/pyenv"
 
-# move to ~/apps/android/bin
-ANDROID_BIN_PATH=$HOME/apps/android-bin/
+addpath() {
+    newelement=${1%/}
+    if [ -d "$1" ] && ! echo $PATH | grep -E -q "(^|:)$newelement($|:)" ; then
+        if [ "$2" = "after" ] ; then
+            PATH="$PATH:$newelement"
+        else
+            PATH="$newelement:$PATH"
+        fi
+    fi
+}
 
-export PATH=$HOME/bin:$PATH
-export PATH=$JAVA_HOME/bin:$PATH
-export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:$ANDROID_BIN_PATH
+rmpath() {
+    PATH="$(echo $PATH | sed -e "s;\(^\|:\)${1%/}\(:\|\$\);\1\2;g" -e 's;^:\|:$;;g' -e 's;::;:;g')"
+}
+
+addpath $HOME/bin after
+addpath $JAVA_HOME/bin after
+addpath $GOPATH/bin after
+
+# move to ~/apps/android/bin
+addpath $HOME/apps/android-bin after
--- zshrc.d/sources/sdkman.zsh
+++ zshrc.d/sources/sdkman.zsh
@@ -2,4 +2,6 @@
 export SDKMAN_DIR="$HOME/.sdkman"
 if [ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]; then
   source "$SDKMAN_DIR/bin/sdkman-init.sh"
+else
+  unset SDKMAN_DIR
 fi
