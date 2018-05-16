8b1ca0b0c7aa7b92d4b19480ea079459ac57e635
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2018-05-16 11:08:06 +0100

    Fix aws-get-ami-region-map

--- zshrc.d/functions/aws.zsh
+++ zshrc.d/functions/aws.zsh
@@ -40,9 +40,10 @@ aws-ecr-login() {
 
 # example:
 # aws-get-region-map --filters 'Name=name,Values=amzn2-ami-hvm-2017.12.0.20180328.1-x86_64-ebs' 'Name=architecture,Values=x86_64'
-aws-get-region-map() {
+aws-get-ami-region-map() {
+  # todo need to somehow add --profile flag to the region retrieval list, for now, export AWS_PROFILE
   regionMap=""
-  for region in $(aws ec2 describe-regions --profile sandbox --query 'Regions[].RegionName' --output text | tr "\t" "\n" | LC_ALL=C sort --human-numeric-sort); do
+  for region in $(aws ec2 describe-regions --query 'Regions[].{Name:RegionName}' --output text | LC_ALL=C sort --human-numeric-sort); do
     images="$(aws ec2 describe-images --owners amazon --region $region "$@" --query 'Images[].ImageId' --output text)"
     imageCount="$(echo "$images" | wc -w | xargs echo -n)"
 
